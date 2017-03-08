9d49bfe9aed6a71acee71e476bc67eb6f63d630f
janmaru <janmaru@gmail.com>
2017-03-08 01:54:09 +0100

    first commit

---
+++ WebHook/IISHelpers.fs
@@ -0,0 +1,12 @@
+﻿namespace  mahamudra.it.iis 
+open System
+    module IISHelpers =
+        /// Port specified by IIS HttpPlatformHandler
+        let httpPlatformPort =
+            match Environment.GetEnvironmentVariable("HTTP_PLATFORM_PORT") with
+            | null -> None
+            | value ->
+                match Int32.TryParse(value) with
+                | true, value -> Some value
+                | false, _ -> None
+
---
+++ WebHook/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Suave
+open mahamudra.it.iis
+
+[<EntryPoint>]
+let main argv =
+    let config = defaultConfig
+    let config =
+        match IISHelpers.httpPlatformPort with
+        | Some port ->
+            { config with
+                bindings = [  HttpBinding.createSimple HTTP "127.0.0.1" port ] }
+        | None -> config
+
+    startWebServer config (Successful.OK "Hello World!")
+    0
